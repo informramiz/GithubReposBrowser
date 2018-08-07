d08da7590c2f70f582638dcb9036cffb1724dece
Ramiz Raja <informramiz@gmail.com>
2018-08-07 12:27:09 +0500

    feat: Test AppExecutors in a Fragment

--- app/src/main/java/io/informramiz/githubreposbrowser/ui/repo/RepoFragment.kt
+++ app/src/main/java/io/informramiz/githubreposbrowser/ui/repo/RepoFragment.kt
@@ -6,10 +6,13 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 
 import io.informramiz.githubreposbrowser.R
+import io.informramiz.githubreposbrowser.common.AppExecutors
 import io.informramiz.githubreposbrowser.data.GithubApiService
 import io.informramiz.githubreposbrowser.di.Injectable
+import java.util.concurrent.Executors
 import javax.inject.Inject
 
 /**
@@ -19,10 +22,19 @@ import javax.inject.Inject
 class RepoFragment : Fragment(), Injectable {
     @Inject
     lateinit var githubApiService: GithubApiService
+    @Inject
+    lateinit var appExecutors: AppExecutors
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_repo, container, false)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        appExecutors.mainThread.run {
+            Toast.makeText(this@RepoFragment.activity, "Run on main thread", Toast.LENGTH_LONG).show()
+        }
     }
 }
